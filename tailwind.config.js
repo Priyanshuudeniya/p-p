5c5347ea9bf9926c5f7fae32eb815cb0bcccfc1b
Priyanshu Udeniya <106472140+Priyanshuudeniya@users.noreply.github.com>
2024-04-15 04:14:01 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: "class",
+    content: ["./*.html"],
+    theme: {
+      extend: {
+  
+        screens: {
+          xs: "480px",
+          ss: "620px",
+          sm: "768px",
+          md: "1060px",
+          lg: "1200px",
+          xl: "1700px",
+        },
+        spacing: {
+          "big": "43rem"
+        },
+      },
+  
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif']
+      },
+    },
+    plugins: [],
+  }
